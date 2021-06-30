7f1204f73de3c572a342dbbd79175d95aa25b8e3
erdldncr <erdldncr@gmail.com>
2021-06-30 09:53:55 +0100

    userPersonalInfo modal completed

--- backend/api/models/userPersonalInfo.js
+++ backend/api/models/userPersonalInfo.js
@@ -73,7 +73,33 @@ const userPersonalInfoSchema=new mongoose.Schema({
     personPhoneNumber:{
         type:Number,
         required:[true,'Please, enter your phone number ']
-    }
+    },
+    personEmailType:String,
+    personEmail:{
+        type:String,
+        required:true,
+        unique:true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    personPhoto:{
+        type:Buffer
+    },
+    personPlatformType:String,
+    personPlatformUserName:String,
+    personCvDoc:{
+        type:String,
+        required:[true,'Please, upload your cv']
+    },
+    personCitizenship:String,
+    personWorkPermit:String,
+    personAvailableFrom:Date,
+    personAvailableTo:String,
+    personDbsDoc:String,
+    personMedicalDoc:String
+
+
+
+
 
 
 
