f26db4a80547ab7ea61fe6d9ce797a5b4dba8ca1
erdldncr <erdldncr@gmail.com>
2021-06-30 23:12:01 +0100

    userPersonalInfo patch method added

--- backend/api/routes/userPersonalInfo.js
+++ backend/api/routes/userPersonalInfo.js
@@ -1,10 +1,12 @@
-const {addUserPersonalInfo} =require('../controllers/userPersonalInfo')
+const {addUserPersonalInfo,updateUserPersonalInfo} =require('../controllers/userPersonalInfo')
 const router=require('express').Router()
 
 router
     .route('/')
             .post(addUserPersonalInfo)
 
-
+router
+        .route('/:id')
+        .patch(updateUserPersonalInfo)
 
 module.exports=router
