ada67f9c6a429f7abc0cf500e1d34792646612fe
erdldncr <erdldncr@gmail.com>
2021-07-01 09:40:57 +0100

    post request fot userInfo

--- backend/api/controllers/userPersonalInfo.js
+++ backend/api/controllers/userPersonalInfo.js
@@ -1,17 +1,19 @@
 const userPersonalInfoSchema=require('../models/userPersonalInfo')
 
 module.exports.addUserPersonalInfo=async(req,res,next)=>{
-
+console.log(req.body)
     try {
         const userPersonalInfo= await userPersonalInfoSchema(req.body)
        
         await userPersonalInfo.save()
+        
         return res.status(200).json({
             message:'User has been added'
         })
     } catch (error) {
+        console.log(error)
         return res.status(500).json({
-            message:'Something went wrong'
+            message:'Something went wrong here'
         })     
     }
 
