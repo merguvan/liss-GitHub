e9e8e7738ab2d7cfaae2b259f3825e2f20700540
erdldncr <erdldncr@gmail.com>
2021-07-01 20:04:46 +0100

    backend folders have been updaded

---
+++ backend/api/routes/user.js
@@ -0,0 +1,25 @@
+const {addUser,authorizeUser,getSingleUser,deleteSingleUSer}=require('../controllers/user/user')
+const {addUserPersonalInfo,updateUserPersonalInfo}=require('../controllers/user/userPersonalInfo')
+const router=require('express').Router()
+
+router
+    .route('/signup')
+            .post(addUser)
+
+router
+        .route('/login')
+        .patch(authorizeUser)
+
+router
+        .route('/userpersonalinfo/:id')
+        .post(addUserPersonalInfo)
+        .patch(updateUserPersonalInfo)
+   
+        
+router
+        .route('/:id')
+        .get(getSingleUser)
+        .delete(deleteSingleUSer)
+ 
+
+module.exports=router
--- src/actions/personalInfoActions.js
+++ src/actions/personalInfoActions.js
@@ -2,8 +2,21 @@ import { ADD_PERSONAL_INFO} from '../actionTypes'
 import axios from 'axios'
 export const addPersonalInfo=(data)=>{
     
-    return dispatch=>(dispatch({
-        type:ADD_PERSONAL_INFO,
-        payload:axios.post('http://localhost:5000/userpersonalinfo',{...data}).then(res=>res.data)}))
+    return dispatch=>(dispatch(
+        axios.post('http://localhost:5000/userpersonalinfo',{data}).then(res=>{
+            return dispatch({
+            type:ADD_PERSONAL_INFO,
+            payload:res.data
+            })
+        })))
 }
 
+// export const getData = (data) => ({
+//     type: GET_USERS,
+//     payload: data,
+//   });
+//   export function getUsers() {
+//     return (dispatch) => {
+//       axios.get(`${API_BASE}`).then((result) => dispatch(getData(result.data)));
+//     };
+//   }
