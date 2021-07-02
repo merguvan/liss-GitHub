69a069ee12c6223ffac4a9d4c42602a77cdbe011
erdldncr <erdldncr@gmail.com>
2021-07-02 12:02:40 +0100

    unique email config and password encryption has been added

--- backend/api/controllers/user/user.js
+++ backend/api/controllers/user/user.js
@@ -1,20 +1,49 @@
+const bcrypt=require('bcrypt')
 const userSchema=require('../../models/user/user')
 
 
 module.exports.addUser=async(req,res)=>{
-    try {
-        const newUser=await new userSchema(req.body)
-        await newUser.save()
-        return res.status(200).json({
-            message:'User has been registered',
-            data:newUser
 
-        })
+    try {
+        const user= await userSchema.findOne({personEmail:req.body.personEmail})
+
+        if(user){
+            return res.status(404).json({
+                message:'This email has already been taken'
+            })
+        }else{
+
+
+    bcrypt.hash(req.body.password,10, async function (err, hash) {
+        if(err) return res.staus(404).json(err)
+ 
+        try {
+         const newUser=await new userSchema({...req.body,password:hash})
+         await newUser.save()
+         return res.status(200).json({
+             message:'User has been registered',
+             data:newUser
+ 
+         })
+     } catch (error) {
+         return res.status(200).json({
+             message:'USer ccouldn\'t be registered',
+             error})
+     }
+     });
+
+        }
+        
     } catch (error) {
-        return res.status(200).json({
-            message:'USer ccouldn\'t be registered',
-            error})
+
+        res.status(404).json({
+            message:'Something went wrong'
+        })
+        
     }
+
+
+   
 
 }
 module.exports.authorizeUser=(req,res)=>{
