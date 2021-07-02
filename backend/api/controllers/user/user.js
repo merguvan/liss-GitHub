d2d5847c34411bbc83aede106ed687b6ab1caea4
erdldncr <erdldncr@gmail.com>
2021-07-02 11:51:07 +0100

    user registration model have been added

--- backend/api/controllers/user/user.js
+++ backend/api/controllers/user/user.js
@@ -1,8 +1,20 @@
 const userSchema=require('../../models/user/user')
 
 
-module.exports.addUser=(req,res)=>{
+module.exports.addUser=async(req,res)=>{
+    try {
+        const newUser=await new userSchema(req.body)
+        await newUser.save()
+        return res.status(200).json({
+            message:'User has been registered',
+            data:newUser
 
+        })
+    } catch (error) {
+        return res.status(200).json({
+            message:'USer ccouldn\'t be registered',
+            error})
+    }
 
 }
 module.exports.authorizeUser=(req,res)=>{
