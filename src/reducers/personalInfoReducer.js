a9d72515141373e143565345de3e2d51e00ea521
erdldncr <erdldncr@gmail.com>
2021-07-07 01:02:28 +0100

    addressInfo reduccer action and actiontypes added

--- src/reducers/personalInfoReducer.js
+++ src/reducers/personalInfoReducer.js
@@ -3,6 +3,11 @@ import {
   ADD_PERSONAL_INFO_REJECTED,
   ADD_PERSONAL_INFO_PENDING,
 } from "../actionTypes/personalInfo";
+import {
+  ADD_ADDRESS_INFO_FULFILLED,
+  ADD_ADDRESS_INFO_REJECTED,
+  ADD_ADDRESS_INFO_PENDING,
+} from "../actionTypes/addressInfo";
 
 const initialState = {
   personalInfo: {},
@@ -30,7 +35,24 @@ export const personalInfoReducer = (state = initialState, action) => {
         loading: false,
         error: action.payload,
       };
+    case ADD_ADDRESS_INFO_PENDING:
+      return {
+        ...state,
+        loading: true,
+      };
 
+    case ADD_ADDRESS_INFO_FULFILLED:
+      return {
+        ...state,
+        loading: false,
+        addressInfo: action.payload,
+      };
+    case ADD_ADDRESS_INFO_REJECTED:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
