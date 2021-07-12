29794f5f2803e74e6a8c20dd4ccfe186e63441f0
erdldncr <erdldncr@gmail.com>
2021-07-12 18:08:57 +0100

    user registration method has been updated

--- backend/api/models/user/user.js
+++ backend/api/models/user/user.js
@@ -28,4 +28,11 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
 module.exports = mongoose.model("user", userSchema);
