77bc334210f7de790e2e21d6537fa06ce0ae9e44
erdldncr <erdldncr@gmail.com>
2021-07-24 09:16:36 +0100

    inout box for inst addedd

--- backend/api/models/user/user.js
+++ backend/api/models/user/user.js
@@ -41,6 +41,10 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
 });
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
