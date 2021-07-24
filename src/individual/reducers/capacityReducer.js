13ddc206b90d78331b8529fa4793adc1da23aad6
Murat <58371531+merguvan@users.noreply.github.com>
2021-07-24 22:21:18 +0100

    capacity and certifications reducers and actionTypes updated

--- src/individual/reducers/capacityReducer.js
+++ src/individual/reducers/capacityReducer.js
@@ -1,21 +1,36 @@
-import { ADD_CAPACITY_DETAILS } from "../actionTypes"
+import {
+  ADD_CAPACITY_INFO_PENDING,
+  ADD_CAPACITY_INFO_REJECTED,
+  ADD_CAPACITY_INFO_FULFILLED,
+} from "../actionTypes/capacity";
 
 const initialState = {
-addCapacity:{},
+  capacity: {},
+  loading: false,
+  error: {},
+};
 
-}
-
-export function capacityReducer (state = initialState,action){
-
-    switch (action.type) {
-
-        case ADD_CAPACITY_DETAILS:
-            
-            return {
-                ...state,
-                addCapacity:action.payload}
+export function capacityReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_CAPACITY_INFO_PENDING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case ADD_CAPACITY_INFO_FULFILLED:
+      return {
+        ...state,
+        loading: false,
+        capacity: action.payload,
+      };
+    case ADD_CAPACITY_INFO_REJECTED:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
 
     default:
-        return state
-    }
+      return state;
+  }
 }
