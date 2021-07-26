75bf2c8e48e01112340a9ba8d69899d663755660
erdldncr <erdldncr@gmail.com>
2021-07-26 02:54:44 +0100

    user-profile-update-redux-connection-completed

--- src/individual/actions/userRegistration.js
+++ src/individual/actions/userRegistration.js
@@ -7,6 +7,9 @@ import {
   USER_REGISTRATION_PENDING,
   USER_REGISTRATION_REJECTED,
   USER_REGISTRATION_FULFILLED,
+  USER_PROFILE_UPDATE_FULFILLED,
+  USER_PROFILE_UPDATE_REJECTED,
+  USER_PROFILE_UPDATE_PENDING,
 } from "../actionTypes/userRegistration";
 export const signup = (data) => async (dispatch) => {
   try {
@@ -44,6 +47,45 @@ export const signup = (data) => async (dispatch) => {
 
     dispatch({
       type: USER_REGISTRATION_REJECTED,
+      payload: message,
+    });
+  }
+};
+
+export const updateUserProfile = (data) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_PROFILE_UPDATE_PENDING,
+    });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      authorization: getState().userLogin.userLogin.token,
+    };
+    const { data: res } = await axios.post(
+      "http://localhost:5000/user/profile",
+      config,
+      data
+    );
+
+    dispatch({
+      type: USER_PROFILE_UPDATE_FULFILLED,
+      payload: res,
+    });
+    dispatch({
+      type: USER_LOGIN_FULFILLED,
+      payload: res,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(res));
+  } catch ({
+    response: {
+      data: { message },
+    },
+  }) {
+    dispatch({
+      type: USER_PROFILE_UPDATE_REJECTED,
       payload: message,
     });
   }
