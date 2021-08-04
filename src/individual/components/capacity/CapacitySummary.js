4a695ce42c224b06045efdb05a81c830e2b58348
Murat <58371531+merguvan@users.noreply.github.com>
2021-08-04 17:26:29 +0100

    capacity and projects summaries completed

--- src/individual/components/capacity/CapacitySummary.js
+++ src/individual/components/capacity/CapacitySummary.js
@@ -7,41 +7,126 @@ import Summary from "../../../Summary";
 import { FiEdit, FiTrash } from "react-icons/fi";
 // import "./Capacity.css";
 
+const capacity = [
+  {
+    personCourseName: "Quality Management",
+    personCourseLanguage: "English",
+    personCourseLevel: "Bachelor's",
+    personAvailableFrom: "2021-10-05",
+    personAvailableTo: "2021-12-05",
+  },
+  {
+    personCourseName: "Educational Technologies",
+    personCourseLanguage: "English",
+    personCourseLevel: "Doctoral",
+    personAvailableFrom: "2021-05-08",
+    personAvailableTo: "2021-08-31",
+  },
+];
+
+const personTheses = [
+  {
+    personThesisLevel: "Doctoral",
+    personSupervisedThesisLanguage: "English",
+  },
+  {
+    personThesisLevel: "Master's",
+    personSupervisedThesisLanguage: "Turkish",
+  },
+];
 
 function Capacity(props) {
   return (
-    <Summary headerTitle="Capacity" url="/capacity/1">
-       <div className="capacity_block">
+    <Summary
+      headerTitle="Teaching / Thesis Supervision Possibilities"
+      url="/capacity/1"
+    >
+      {capacity.map((i, index) => {
+        return (
+          <div className="work_experience_block">
             <div className="person-summary-body-title"></div>
             <div className="person-summary-body-context-container">
-              <h6 id="work_h6">
-                <div className="person-summary-body-context-container_level_1">
+              {index === 0 ? (
+                <h6 id="work_h6">
+                  <div className="person-summary-body-context-container_level_1">
+                    <p>Courses that can be offered</p>
+                  </div>
+                </h6>
+              ) : (
+                <span></span>
+              )}
+              {index === 0 ? (
+                <div className="person-summary-body-icon-container">
+                  <span>
+                    <Link to={"/"}>
+                      <FiEdit />
+                    </Link>
+                  </span>
+                  <span>
+                    <FiTrash />
+                  </span>
+                </div>
+              ) : (
+                ""
+              )}
+
+              {i.personCourseName !== "" ? (
+                <div className="person-summary-body-context-container_level_2">
                   <p>
-                    Hpaskjdpıasjkd
+                    <em>
+                      {i.personCourseName}
+                      {i.personCourseLanguage !== ""
+                        ? " in " + i.personCourseLanguage
+                        : ""}
+                      {i.personCourseLevel !== ""
+                        ? ", " + i.personCourseLevel + " Level"
+                        : ""}
+                    </em>
                   </p>
+                  {i.personAvailableFrom !== "" ? (
+                    <p>
+                      Avaliable to teach:{" "}
+                      <em>
+                        {i.personAvailableFrom} - {i.personAvailableTo}
+                      </em>
+                    </p>
+                  ) : (
+                    ""
+                  )}
                 </div>
-              </h6>
-              <div className="person-summary-body-icon-container">
-                <span>
-                  <Link to={"/"}>
-                    <FiEdit />
-                  </Link>
-                </span>
-                <span>
-                  <FiTrash />
-                </span>
-              </div>
-              <div className="person-summary-body-context-container_level_2">
-                <p>asdasdqwd</p>
-              </div>
-              <div className="person-summary-body-context-container_level_3">
-                <div>
-                  {" "}
-                  <p>asdasdsadasd</p>
-                </div>
-              </div>
+              ) : (
+                <span></span>
+              )}
             </div>
           </div>
+        );
+      })}
+      <h6 id="work_h6">
+        <div className="person-summary-body-context-container_level_1">
+          <p>Supervision Possibilities</p>
+        </div>
+      </h6>
+      <div className="person-summary-body-icon-container">
+        <span>
+          <Link to={"/"}>
+            <FiEdit />
+          </Link>
+        </span>
+        <span>
+          <FiTrash />
+        </span>
+      </div>
+      {personTheses.length > 0 ? (
+        <div className="person-summary-body-context-container_level_2">
+          <p>
+            {personTheses.map((i, index) =>  
+            index > 0 ? ", " + i.personThesisLevel + " Level": i.personThesisLevel + " Level")
+            }
+          </p>
+        </div>
+      ) : (
+        ""
+      )}
     </Summary>
   );
 }
--- src/individual/components/projects/ProjectsSummary.js
+++ src/individual/components/projects/ProjectsSummary.js
@@ -7,18 +7,36 @@ import Summary from "../../../Summary";
 import { FiEdit, FiTrash } from "react-icons/fi";
 // import "./Projects.css";
 
-
+const projects = [
+  {
+    personProjectRole: "Institutional Coordinator",
+    personProjectName:
+      "Raising Research Capacity of Georgian HEls through Developing R&O Units",
+    personProjectGrantProvider: "EU Erasmus+ KA2",
+    personProjectNumber: "HERD- 598207-EPP-1-2018-1-CE-EPPKA2-CBHE-SP",
+    personProjectFrom: "2019-01-01",
+    personProjectTo: "2021-01-01",
+  },
+  {
+    personProjectRole: "Project Coordinator",
+    personProjectName: "Developing Software for Project Management",
+    personProjectGrantProvider: "Soft Innovas",
+    personProjectNumber: "SI-2018-PR-001",
+    personProjectFrom: "2018-01-01",
+    personProjectTo: "2021-02-03",
+  },
+];
 function Projects(props) {
   return (
     <Summary headerTitle="Projects" url="/projects/1">
-       <div className="projects_block">
+      {projects.map((i) => {
+        return (
+          <div className="work_experience_block">
             <div className="person-summary-body-title"></div>
             <div className="person-summary-body-context-container">
               <h6 id="work_h6">
                 <div className="person-summary-body-context-container_level_1">
-                  <p>
-                    Hpaskjdpıasjkd
-                  </p>
+                  <p>{i.personProjectRole}</p>
                 </div>
               </h6>
               <div className="person-summary-body-icon-container">
@@ -32,16 +50,34 @@ function Projects(props) {
                 </span>
               </div>
               <div className="person-summary-body-context-container_level_2">
-                <p>asdasdqwd</p>
+                <p>{i.personProjectName}</p>
+                {i.personProjectGrantProvider !== "" ? (
+                  <em>Granted by: {i.personProjectGrantProvider}</em>
+                ) : (
+                  <span></span>
+                )}
+                {i.personProjectNumber !== "" ? (
+                  <em>
+                    {i.personProjectGrantProvider !== "" ? <br></br> : <span></span>}
+                    Project Number: {i.personProjectNumber}
+                  </em>
+                ) : (
+                  <span></span>
+                )}
               </div>
               <div className="person-summary-body-context-container_level_3">
                 <div>
-                  {" "}
-                  <p>asdasdsadasd</p>
+                  <p>
+                    {i.personProjectFrom !== "" ? i.personProjectFrom : <span></span>}
+                    {i.personProjectTo !== "" ? (
+                        " - " + i.personProjectTo) : <span></span>}
+                  </p>
                 </div>
               </div>
             </div>
           </div>
+        );
+      })}
     </Summary>
   );
 }
