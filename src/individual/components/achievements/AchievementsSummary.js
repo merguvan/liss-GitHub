b72943661996d41b379bbb5ef7bb7ab17e01070e
SaidYein <saidyein@gmail.com>
2021-08-04 10:57:10 +0200

    achievements summary finished

--- src/individual/components/achievements/AchievementsSummary.js
+++ src/individual/components/achievements/AchievementsSummary.js
@@ -7,42 +7,53 @@ import Summary from "../../../Summary";
 import { FiEdit, FiTrash } from "react-icons/fi";
 // import "./Achievements.css";
 
-
+const achievements =[
+  {
+    personAchievementGrantedBy:"BP",
+    personAchievementCountry:"England",
+    personAchievementName:"Fellowship",
+    personAchievementDate:"20-09-2006"
+  },
+  {
+    personAchievementGrantedBy:"Booking.com",
+    personAchievementCountry:"The Netherlands",
+    personAchievementName:"Partnership",
+    personAchievementDate:"20-09-2007"
+  }
+]
 function Achievements(props) {
   return (
-    <Summary headerTitle="Achievements" url="/achievements/1">
-       <div className="achievements_block">
-            <div className="person-summary-body-title"></div>
-            <div className="person-summary-body-context-container">
-              <h6 id="work_h6">
-                <div className="person-summary-body-context-container_level_1">
-                  <p>
-                    Hpaskjdpıasjkd
-                  </p>
-                </div>
-              </h6>
-              <div className="person-summary-body-icon-container">
-                <span>
-                  <Link to={"/"}>
-                    <FiEdit />
-                  </Link>
-                </span>
-                <span>
-                  <FiTrash />
-                </span>
+    <Summary headerTitle="Achievements" url="/Achievements/1">
+    {achievements.map((i) => {
+      return (
+        <div className="work_experience_block">
+          <div className="person-summary-body-title"></div>
+          <div className="person-summary-body-context-container">
+            <h6 id="work_h6">
+              <div className="person-summary-body-context-container_level_1">
+                <p>
+                  {i.personAchievementName}
+                </p>
               </div>
-              <div className="person-summary-body-context-container_level_2">
-                <p>asdasdqwd</p>
-              </div>
-              <div className="person-summary-body-context-container_level_3">
-                <div>
-                  {" "}
-                  <p>asdasdsadasd</p>
-                </div>
-              </div>
+            </h6>
+            <div className="person-summary-body-icon-container">
+              <span>
+                <Link to={"/"}>
+                  <FiEdit />
+                </Link>
+              </span>
+              <span>
+                <FiTrash />
+              </span>
+            </div>
+            <div className="person-summary-body-context-container_level_2">
+              <p><em>Granted by {i.personAchievementGrantedBy} on {i.personAchievementDate} in {i.personAchievementCountry}</em></p>
             </div>
           </div>
-    </Summary>
+        </div>
+      );
+    })}
+  </Summary>
   );
 }
 
