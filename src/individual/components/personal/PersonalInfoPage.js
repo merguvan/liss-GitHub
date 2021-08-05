31f157423ade1715ca8974532fc71f4287e220e3
SaidYein <saidyein@gmail.com>
2021-08-05 12:08:03 +0200

    tabs

--- src/individual/components/personal/PersonalInfoPage.js
+++ src/individual/components/personal/PersonalInfoPage.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import { Redirect, useParams } from "react-router-dom";
 import PersonInfo from "./personalInfo";
-import AddressInfo from "./AddressInfo";
+import AddressInfo from "./AddressInfo/index";
 
 export default function PersonalInfo(props) {
   const { id } = useParams();
-
+  console.log(id)
   switch (+id) {
     case 1:
-      return <PersonInfo />;
-    case 2:
       return <AddressInfo />;
+
 
     default:
       <Redirect to="/" />;
