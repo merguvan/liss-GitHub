4bd6c15056e2a063a3efba86c72880cc45792836
Yusup-web <yusup.atdayev@mail.ru>
2021-08-12 23:53:46 +0300

    Actions CapacityActions Info

--- src/individual/actions/capacityAction.js
+++ src/individual/actions/capacityAction.js
@@ -3,6 +3,12 @@ import {
   ADD_CAPACITY_INFO_PENDING,
   ADD_CAPACITY_INFO_REJECTED,
   ADD_CAPACITY_INFO_FULFILLED,
+  UPDATE_CAPACITY_INFO_PENDING,
+  UPDATE_CAPACITY_INFO_FULFILLED,
+  UPDATE_CAPACITY_INFO_REJECTED,
+  GET_CAPACITY_INFO_PENDING,
+  GET_CAPACITY_INFO_FULFILLED,
+  GET_CAPACITY_INFO_REJECTED,
 } from "../actionTypes/capacity";
 
 const {
@@ -16,34 +22,90 @@ const {
         _id: 12345
     }};
 
-export const addCapacity = (capacity) => async (dispatch) => {
-  console.log("token=>" + token);
-  console.log("id=>" + id);
-  console.log(capacity);
+export const addCapacityInfo = (data) => async (dispatch, getState) => {
+  const { token, _id: id } = getState().userLogin.userLogin;
+
   try {
     dispatch({
       type: ADD_CAPACITY_INFO_PENDING,
     });
-    const { data } = await axios.post(
-      `http://localhost:5000/user/capacity/${id}`,
-      { ...capacity, user: id },
+
+    const { data: res } = await axios.post(
+      `http://localhost:5000/user/capacityinfo/`,
+      { ...data, user: id },
       {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       }
     );
-    console.log(data);
     dispatch({
       type: ADD_CAPACITY_INFO_FULFILLED,
-      payload: data.capacity,
+      payload: res,
+    });
+  } catch ({
+    response: {
+      data: { message },
+    },
+  }) {
+    dispatch({
+      type: ADD_CAPACITY_INFO_REJECTED,
+      payload: message,
+    });
+  }
+};
+
+export const updateCapacityInfo = (data) => async (dispatch, getState) => {
+  const { token, id } = getState().userLogin;
+  try {
+    dispatch({
+      type: UPDATE_CAPACITY_INFO_PENDING,
+    });
+    const { data: res } = await axios.patch(
+      `http://localhost:5000/user/capacityinfo/`,
+      { data },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    dispatch({
+      type: UPDATE_CAPACITY_INFO_FULFILLED,
+      payload: res,
     });
   } catch (error) {
     dispatch({
-      type: ADD_CAPACITY_INFO_REJECTED,
+      type: UPDATE_CAPACITY_INFO_REJECTED,
       payload: error,
     });
-    console.log(error);
+  }
+};
+export const getCapacityInfo = (data) => async (dispatch, getState) => {
+  const { token } = getState().userLogin;
+
+  try {
+    dispatch({
+      type: GET_CAPACITY_INFO_PENDING,
+    });
+    const { data: res } = await axios.patch(
+      `http://localhost:5000/user/capacityinfo/`,
+      { data },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    dispatch({
+      type: GET_CAPACITY_INFO_FULFILLED,
+      payload: res,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_CAPACITY_INFO_REJECTED,
+      payload: error,
+    });
   }
 };
 
