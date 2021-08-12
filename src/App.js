70620b09ce2090165ed7549aab9b6379d20c78cd
erdldncr <erdldncr@gmail.com>
2021-08-12 13:30:05 +0100

    app js true added

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ import { useEffect } from "react";
 function App(props) {
   const userInfo = useSelector((state) => state.userLogin?.userLogin);
 
-  if (userInfo?.isConfirmed) {
+  if (true) {
     return (
       <div className="app">
         <Header />
